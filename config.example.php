634ae2d8d6798a53efb99a0385d378c33b0a2107
ijazabd41 <ijazabd41@gmail.com>
2026-07-23 01:06:46 +0500

    Scrub config.php from git history and add config.example.php

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+// CONFIG - update these values only
+$ODOO_BASE_URL = 'http://cooperp.freeddns.org:8070';
+$ODOO_API_KEY  = 'YOUR_API_KEY';
+$ODOO_DB       = 'production'; //staging-apr17 for 8076 production for 8070
+
+
+
+// Website URLs
+$SITE_BASE_URL = 'https://coop-discounts.com';
+$CURRENCY = 'AED';
